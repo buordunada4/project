5d3d1f52428498cbf3e697dd63fe868bdd92a3ac
buordunada4 <157094786+buordunada4@users.noreply.github.com>
2024-01-19 21:20:43 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,25 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        body: Center(
-          child: Text(
-            'Kamilla',
-            style: TextStyle(
-              color: Colors.purple,
-              fontSize: 36.0,
-              fontWeight: FontWeight.bold,
-            ),
+    return MaterialApp(
+        color: Colors.blue,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 130, 220, 133),
           ),
-        ),
-      ),
-    );
+          body: Container(
+              color: const Color.fromARGB(255, 177, 14, 14),
+              child: Text("i'm Ansar")),
+        ));
   }
 }
